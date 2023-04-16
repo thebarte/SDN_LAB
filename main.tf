89ded1bb09493747307b1c8b97d49b26ae71beb1
Bart <77812900+thebarte@users.noreply.github.com>
2023-04-16 14:44:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 1.1.0"
-
-  cloud {
-    organization = "Terraform_Project_TELE36058"
-
-    workspaces {
-      name = "SDN_production"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 module "vpc" {
   source = "./vpc"
   vpc_cidr = "192.168.0.0/16"
