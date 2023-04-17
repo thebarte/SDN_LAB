ea36af26876ddb566579a8b40582e29b556eab11
Bart <77812900+thebarte@users.noreply.github.com>
2023-04-17 19:48:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source   = "./vpc"
+  source = "./vpc"
   vpc_cidr = "192.168.0.0/16"
   subnet_cidrs = [
     "192.168.1.0/24",
@@ -11,18 +11,16 @@ module "vpc" {
     "us-east-1b",
     "us-east-1c"
   ]
-}
+} 
 module "security_group" {
   source = "./security_group"
   vpc_id = module.vpc.vpc_id
 }
 
 module "high_availability_infrastructure" {
-  source            = "./high_availability_infrastructure"
-  subnets           = module.vpc.subnet_ids
-  ami_id            = "ami-0ee1b569239bf6c3e" # Replace with your custom AMI ID
-  vpc_id            = module.vpc.vpc_id
+  source = "./high_availability_infrastructure"
+  subnets = module.vpc.subnet_ids
+  ami_id = "ami-0ee1b569239bf6c3e" # Replace with your custom AMI ID
+  vpc_id = module.vpc.vpc_id
   security_group_id = module.security_group.security_group_id
 }
-    
-    
